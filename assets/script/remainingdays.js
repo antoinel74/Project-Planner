5592d02e6a577a338b4bd6674a21ef269dd01b2c
antoinel74 <antoine.lansman@gmail.com>
2023-10-03 14:35:17 +0200

    init var + add main title

---
+++ assets/script/remainingdays.js
@@ -0,0 +1,11 @@
+let inputDate = document.querySelector('input[type="date"]');
+inputDate = new Date("11/23/2023");
+let todayDate = new Date();
+
+console.log(inputDate); //TEST
+console.log(todayDate); // TEST
+
+let differenceTime = inputDate.getTime() - todayDate.getTime();
+let differenceDays = Math.floor(differenceTime / (1000 * 3600 * 24));
+
+console.log(differenceDays + " days remaining"); // TEST
