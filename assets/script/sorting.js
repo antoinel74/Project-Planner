f915926438ffe0d32e3d01486b771c17b0564023
Justin Michel <justinmichel2000@gmail.com>
2023-10-04 11:53:40 +0200

    test

--- assets/script/sorting.js
+++ assets/script/sorting.js
@@ -1,12 +1,53 @@
-function sortTasks() {
-  let sortBy = sortSelect.value;
+// const sortSelect = document.querySelectorAll(".sort-option");
+
+
+// function sortTasks(card) {
+//   let sortBy = sortSelect.value;
+//   if (sortBy === "name") {
+//     card.sort((a, b) => a.name.localeCompare(b.name));
+//   } else if (sortBy === "remainingTime") {
+//     card.sort((a, b) => a.dueDate.localeCompare(b.dueDate));
+//   }
+//   // updateTasksDisplay();
+// }
+
+
+// sortSelect.addEventListener("change", sortTasks);
+// // sortTasks.addEventListener("change", sortSelect);
+
+
+
+
+
+
+
+
+const sortSelect = document.querySelectorAll("#sort-select");  
+
+function sortTasks(cardArray) {
+  // console.log(sortSelect.value);
+  let sortBy = this.value;
   if (sortBy === "name") {
-    tasks.sort((a, b) => a.name.localeCompare(b.name));
+    cardArray.sort((a, b) => a.name.localeCompare(b.name));
   } else if (sortBy === "remainingTime") {
-    tasks.sort((a, b) => a.dueDate.localeCompare(b.dueDate));
+    cardArray.sort((a, b) => a.dueDate.localeCompare(b.dueDate));
   }
-  updateTasksDisplay();
+ 
+  updateTasksDisplay(cardArray);
 }
 
 
-sortSelect.addEventListener("change", sortTasks);
+sortSelect.forEach(option => {
+  option.addEventListener("change", function () {
+    
+    const cardArray = Array.from(sortSelect);
+    sortTasks.call(this, cardArray);
+    
+  });
+});
+
+
+
+function updateTasksDisplay(cardArray) {
+ 
+};
