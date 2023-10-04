e0e189685b48f4208b5b7365e236d22428669a25
tim <timdesmet00@gmail.com>
2023-10-04 15:42:31 +0200

    style modale et card modif

--- assets/script/modale.js
+++ assets/script/modale.js
@@ -2,12 +2,16 @@ const btnModale = document.querySelector(".button-modale");
 const bodyTarget = document.querySelector("body");
 const headerTarget = bodyTarget.querySelector("header");
 const createCardButton = document.getElementById("create-card-button");
-// const hiddenTarget = bodyTarget.querySelector("#modale");
+const closeModale = document.getElementById("close-modale");
 
 btnModale.addEventListener("click", () => {
   headerTarget.classList.remove("hidden");
 });
 
 createCardButton.addEventListener("click", () => {
+  headerTarget.classList.add("hidden");
+});
+
+closeModale.addEventListener("click", () => {
   headerTarget.classList.add("hidden");
 });
