cc1bc56b225b7a55d7f1a567fc4919ca38a91f8b
antoinel74 <antoine.lansman@gmail.com>
2023-10-05 16:32:49 +0200

    final merge

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# project-planner
+#Project-Planner
--- main.js
+++
@@ -1,5 +0,0 @@
-import { card } from "./assets/script/card.js";
-
-let taskStates = document.getElementsByClassName("filter");
-taskStates.addEventListener("click", () => console.log("coucou"));
-console.log();
